e2e07f7b47a295021117b6a5ed12346315a0b42d
xakyth <xakyth@gmail.com>
2023-05-24 20:03:01 +0300

    initial commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import classes.Multiplication;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String result = Multiplication.simpleIntMult("99999", "9999");
+        System.out.println(result);
+    }
+}
+
+//TODO: RecIntMult, Karatsuba mult
+//Selection sort, insertion sort, bubble sort, merge sort
+
+//TODO: test cases
---
+++ src/classes/Multiplication.java
@@ -0,0 +1,51 @@
+package classes;
+
+public class Multiplication {
+
+    public static String simpleIntMult(String a, String b) {
+        String[] summands = new String[b.length()];
+        int carry = 0;
+
+        for (int i = b.length() - 1, k = 0; i >= 0; i--, k++) {
+            StringBuilder summand = new StringBuilder();
+            for (int j = 0; j < k; j++) {
+                summand.append('0');
+            }
+            carry = 0;    
+            for (int j = a.length() - 1; j >= 0; j--) {
+                int temp = (b.charAt(i) - '0') * (a.charAt(j) - '0') + carry;
+                summand.append(temp % 10);
+                carry = temp / 10;
+            }
+            if (carry > 0) {
+                summand.append(carry);
+            }
+            summands[k] = summand.toString();
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        int n = summands[summands.length - 1].length();
+        carry = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < summands.length; j++) {
+                if (summands[j].length() > i) {
+                    sum += (summands[j].charAt(i) - '0');
+                }
+            }
+            sum += carry;
+            result.append(sum % 10);
+            carry = sum / 10;      
+        }
+        while (carry > 0) {
+            result.append(carry % 10);
+            carry /= 10;
+        }
+
+        return result.reverse().toString();
+    }
+
+    
+
+}

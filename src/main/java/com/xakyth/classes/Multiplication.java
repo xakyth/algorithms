cb4188242b418808864dcc8017071e6335c8bba8
xakyth <xakyth@gmail.com>
2023-05-28 11:43:21 +0300

    added implementation of Karatsuba Multiplication

--- src/main/java/com/xakyth/classes/Multiplication.java
+++ src/main/java/com/xakyth/classes/Multiplication.java
@@ -2,6 +2,8 @@ package com.xakyth.classes;
 
 import java.math.BigInteger;
 
+import javafx.util.Pair;
+
 public class Multiplication {
 
     public static String simpleIntMult(String a, String b) {
@@ -80,6 +82,53 @@ public class Multiplication {
         }
 
         return new impl()._intRecMult(x, y).replaceAll("^0+(?!$)", "");
+    }
+
+    public static String karatsubaMult(String x, String y) {
+        class impl {
+            Pair<String, String> equalLengthPow2(String x, String y) {
+                //check if x.length() and y.length() are both power of 2,
+                //pad x and y with zeros to left until length isn't power of 2
+                //and make their length equal
+                int maxLength = Math.max(x.length(), y.length());
+                int desiredLength = Integer.highestOneBit(maxLength);
+                if ((maxLength & (maxLength - 1)) != 0) 
+                     desiredLength *= 2;
+                return new Pair<>(String.format("%" + desiredLength + "s", x).replace(' ', '0'), String.format("%" + desiredLength + "s", y).replace(' ', '0'));
+            }
+
+            String _karatsubaMult(String x, String y) {
+                int n = x.length();
+                //base case
+                if (n == 1) {
+                    return String.valueOf(Integer.parseInt(x) * Integer.parseInt(y));
+                }
+                String a = x.substring(0, n/2);
+                String b = x.substring(n/2);
+                String c = y.substring(0, n/2);
+                String d = y.substring(n/2);
+
+                String ac = _karatsubaMult(a, c);
+                String bd = _karatsubaMult(b, d);
+                //10^(n/2) * (ad + bc) == 10^(n/2) * ((a+b)*(c+d) - ac - bd)
+
+                Pair<String, String> ab_cd = this.equalLengthPow2(String.valueOf(new BigInteger(a).add(new BigInteger(b))),
+                                            String.valueOf(new BigInteger(c).add(new BigInteger(d))));
+                String adbc = _karatsubaMult(ab_cd.getKey(), ab_cd.getValue());
+                adbc = String.valueOf(new BigInteger(adbc).subtract(new BigInteger(ac).add(new BigInteger(bd))));
+                adbc += String.format("%" + n/2 + "s", "").replace(' ', '0');
+                
+                //10^n(ac)
+                ac += String.format("%" + n + "s", "").replace(' ', '0');
+                
+                //10^n(ac) + 10^(n/2)(ad + bc) + bd
+                return String.valueOf((new BigInteger(ac).add(new BigInteger(adbc)).add(new BigInteger(bd))));
+            }
+        }
+
+        impl implementation = new impl();
+        Pair<String, String> newXY = implementation.equalLengthPow2(x, y);
+        return implementation._karatsubaMult(newXY.getKey(), newXY.getValue()).replaceAll("^0+(?!$)", "");
     }
 
 }
--- src/test/java/com/xakyth/MultiplicationTest.java
+++ src/test/java/com/xakyth/MultiplicationTest.java
@@ -36,4 +36,23 @@ public class MultiplicationTest {
         assertEquals("1219253925", Multiplication.intRecMult("12345", "98765"));
         assertEquals("1219326311126352690", Multiplication.intRecMult("123456789", "9876543210"));
     }
+
+    @Test
+    void karatsubaMultTest() {
+        assertEquals("999890001", Multiplication.karatsubaMult("99999", "9999"));
+        assertEquals("1", Multiplication.karatsubaMult("1", "1"));
+        assertEquals("0", Multiplication.karatsubaMult("1", "0"));
+        assertEquals("0", Multiplication.karatsubaMult("0", "1"));
+        assertEquals("2", Multiplication.karatsubaMult("2", "1"));
+        assertEquals("2", Multiplication.karatsubaMult("1", "2"));
+        assertEquals("8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184", Multiplication.karatsubaMult("3141592653589793238462643383279502884197169399375105820974944592", "2718281828459045235360287471352662497757247093699959574966967627"));
+        assertEquals("52", Multiplication.karatsubaMult("4", "13"));
+        assertEquals("52", Multiplication.karatsubaMult("13", "4"));
+        assertEquals("1219253925", Multiplication.karatsubaMult("12345", "98765"));
+        assertEquals("1219326311126352690", Multiplication.karatsubaMult("123456789", "9876543210"));
+        assertEquals("12221", Multiplication.karatsubaMult("1111", "11"));
+        assertEquals("11110", Multiplication.karatsubaMult("1111", "10"));
+        assertEquals("111100", Multiplication.karatsubaMult("1111", "100"));
+        assertEquals("1111", Multiplication.karatsubaMult("1111", "1"));
+    }
 }

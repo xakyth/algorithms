05db27f2c08a948d343bd4c5695cc58c305b031a
xakyth <xakyth@gmail.com>
2023-05-29 21:00:42 +0300

    stub file to count inversions in an array

---
+++ src/main/java/com/xakyth/InversionsCount.java
@@ -0,0 +1,9 @@
+package com.xakyth;
+
+public class InversionsCount {
+    
+    //TODO:
+    public static int inversionsCount(int[] A) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/xakyth/InversionsCountTest.java
@@ -0,0 +1,11 @@
+package com.xakyth;
+
+import org.junit.jupiter.api.Test;
+
+public class InversionsCountTest {
+    //TODO:
+    @Test
+    void testInversionsCount() {
+
+    }
+}

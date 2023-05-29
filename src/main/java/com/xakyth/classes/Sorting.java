0a91ad625e458be7a3119e5111c86ece610f1bb6
xakyth <xakyth@gmail.com>
2023-05-29 20:35:12 +0300

    implemented merge sorting

--- src/main/java/com/xakyth/classes/Sorting.java
+++ src/main/java/com/xakyth/classes/Sorting.java
@@ -1,5 +1,38 @@
 package com.xakyth.classes;
 
 public class Sorting {
-    //TODO: 
+    
+    public static int[] mergeSort(int[] A) {
+        
+        class impl { 
+            int[] mergeSort(int[] A, int l, int r) {
+                if (l >= r) {
+                    return A;
+                }
+                int mid = (l + r) / 2;
+                mergeSort(A, l, mid);
+                mergeSort(A, mid + 1, r);
+                return this.merge(A, l, mid, r);
+            }
+            
+            int[] merge(int[] A, int l, int mid, int r) {
+                int[] merged = new int[r - l + 1];
+                int i = l, j = mid + 1;
+                for (int k = 0; k < merged.length; k++) {
+                    if (j > r || (i <= mid && A[i] <= A[j])) {
+                        merged[k] = A[i];
+                        i++;
+                    } else {
+                        merged[k] = A[j];
+                        j++;
+                    }
+                }
+                System.arraycopy(merged, 0, A, l, merged.length);
+                return A;
+            }
+        }
+        
+        return new impl().mergeSort(A, 0, A.length-1);
+        
+    }
 }
--- src/test/java/com/xakyth/SortingTest.java
+++ src/test/java/com/xakyth/SortingTest.java
@@ -1,5 +1,25 @@
 package com.xakyth;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.xakyth.classes.Sorting;
+
 public class SortingTest {
     
+    @Test
+    void mergeSortTest() {
+        assertArrayEquals(new int[] { 1, 2, 3, 4 }, Sorting.mergeSort(new int[] { 4, 3, 2, 1}));
+        assertArrayEquals(new int[0], Sorting.mergeSort(new int[0]));
+        assertArrayEquals(new int[] { 1 }, Sorting.mergeSort(new int[] { 1 }));
+        assertArrayEquals(new int[] { 1, 2 }, Sorting.mergeSort(new int[] { 1, 2 }));
+        assertArrayEquals(new int[] { 1, 2 }, Sorting.mergeSort(new int[] { 2, 1 }));
+        assertArrayEquals(new int[] { 1, 2, 3 }, Sorting.mergeSort(new int[] { 3, 2, 1}));
+        assertArrayEquals(new int[] { 1, 2, 3, 7, 11 }, Sorting.mergeSort(new int[] { 3, 7, 2, 1, 11 }));
+        assertArrayEquals(new int[] { -5, 1, 2, 3, 7, 11 }, Sorting.mergeSort(new int[] { 3, 7, 2, -5, 1, 11 }));
+        assertArrayEquals(new int[] { -5, 1, 2, 3, 5, 7, 11 }, Sorting.mergeSort(new int[] { 3, 7, 2, -5, 1, 11, 5 }));
+        assertArrayEquals(new int[] { -5, 1, 2, 3, 5, 7, 11, 99 }, Sorting.mergeSort(new int[] { 99, 3, 7, 2, -5, 1, 11, 5 }));
+        assertArrayEquals(new int[] { -5, 0, 1, 2, 3, 5, 7, 11, 99 }, Sorting.mergeSort(new int[] { 99, 3, 7, 0, 2, -5, 1, 11, 5 }));
+    }
 }

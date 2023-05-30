40b0610293af0f649aa0fc8e772d64d4ad51a05d
xakyth <xakyth@gmail.com>
2023-05-30 06:59:06 +0300

    added implementation of inversions count

--- src/main/java/com/xakyth/InversionsCount.java
+++ src/main/java/com/xakyth/InversionsCount.java
@@ -2,8 +2,33 @@ package com.xakyth;
 
 public class InversionsCount {
     
-    //TODO:
     public static int inversionsCount(int[] A) {
-        return 0;
+        class impl {
+            int InvSumAndMerge(int[] A, int l, int r) {
+                if (l >= r) {
+                    return 0;
+                }
+                int mid = (l + r) / 2;
+                return InvSumAndMerge(A, l, mid) + InvSumAndMerge(A, mid+1, r) + merge(A, l, mid, r);
+            }
+
+            int merge(int[] A, int l, int mid, int r) {
+                int[] merged = new int[r - l + 1];
+                int i = l, j = mid + 1, invCount = 0;
+                for (int k = 0; k < merged.length; k++) {
+                    if (j > r || (i <= mid && A[i] <= A[j])) {
+                        merged[k] = A[i];
+                        i++;
+                    } else {
+                        merged[k] = A[j];
+                        j++;
+                        invCount += (mid - i + 1);
+                    }
+                }
+                System.arraycopy(merged, 0, A, l, merged.length);
+                return invCount;
+            }
+        }
+        return new impl().InvSumAndMerge(A, 0, A.length-1);
     }
 }
--- src/test/java/com/xakyth/InversionsCountTest.java
+++ src/test/java/com/xakyth/InversionsCountTest.java
@@ -1,11 +1,28 @@
 package com.xakyth;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class InversionsCountTest {
-    //TODO:
+    
     @Test
     void testInversionsCount() {
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { }));
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { 0 }));
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { -1 }));
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { 1 }));
 
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { 0, 1 }));
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { 1, 1 }));
+        assertEquals(1, InversionsCount.inversionsCount(new int[] { 2, 1 }));
+            
+        assertEquals(1, InversionsCount.inversionsCount(new int[] { -1, 2, 1 }));
+        assertEquals(3, InversionsCount.inversionsCount(new int[] { 4, 2, 1 }));
+        assertEquals(2, InversionsCount.inversionsCount(new int[] { 2, 3, 1 }));
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { -1, 0, 1 }));
+
+        assertEquals(0, InversionsCount.inversionsCount(new int[] { -1, 0, 1, 7, 10 }));
+        assertEquals(4, InversionsCount.inversionsCount(new int[] { 11, 0, 1, 7, 10 }));
     }
 }

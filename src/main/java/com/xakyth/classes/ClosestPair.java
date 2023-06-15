84746c6af7094146d1703057f4a497d71b80b8c9
xakyth <xakyth@gmail.com>
2023-06-15 07:20:26 +0300

    added base case, compute distance and returning best of 3

--- src/main/java/com/xakyth/classes/ClosestPair.java
+++ src/main/java/com/xakyth/classes/ClosestPair.java
@@ -12,11 +12,22 @@ public class ClosestPair {
         class implementation {
             Pair<Point, Point> search(Point[] P_x, Point[] P_y) {
                 int n = P_x.length;
-
-                //base case
+                //base case n == 2 || n == 3
                 if (n <= 3) {
-                    //TODO:
-                    return null;
+                    Pair<Point, Point> result = new Pair<Point,Point>(P_x[0], P_x[1]);
+                    if (n == 3) {
+                        double bestD = computeDistance(P_x[0], P_x[1]);
+                        double d = computeDistance(P_x[0], P_x[2]);
+                        if (d < bestD) {
+                            bestD = d;
+                            result = new Pair<Point, Point>(P_x[0], P_x[2]);
+                        }
+                        d = computeDistance(P_x[1], P_x[2]);
+                        if (d < bestD) {
+                            result = new Pair<Point, Point>(P_x[1], P_x[2]);
+                        }
+                    }
+                    return result;
                 } else {
                     Point[] L_x = new Point[n / 2];
                     Point[] L_y = new Point[n / 2];
@@ -34,17 +45,29 @@ public class ClosestPair {
                             r++;
                         }
                     }
-                    //TODO: find best of 3
                     Pair<Point, Point> leftPair = search(L_x, L_y);
                     Pair<Point, Point> rightPair = search(R_x, R_y);
-                    Pair<Point, Point> splitPair = searchSplitPair(P_x, P_y, 0, rightPair);
-                    return null;
+                    double d1 = computeDistance(leftPair.getKey(), leftPair.getValue());
+                    double d2 = computeDistance(rightPair.getKey(), rightPair.getValue());
+                    Pair<Point, Point> splitPair = null;
+                    if (d1 <= d2) {
+                        splitPair = searchSplitPair(P_x, P_y, d1, leftPair);
+                    } else {
+                        splitPair = searchSplitPair(P_x, P_y, d2, rightPair);
+                    }
+                    return splitPair;
                 }
             }
             //TODO: implement serachSplitPair
             public Pair<Point, Point> searchSplitPair(Point[] P_x, Point[] P_y, double d, Pair<Point, Point> bestPair) {
-
+                double median_x = P_x[(P_x.length % 2 == 0) ? P_x.length / 2 - 1 : P_x.length].x;
+                for (int i = 0; i < P_y.length; i++) {
+                    
+                }
                 return bestPair;
+            }
+            public double computeDistance(Point p1, Point p2) {
+                return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
             }
         }
         //cloning P_x and P_y from P. P_x sorted by X, P_y sorted by Y

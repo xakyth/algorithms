df72bddeb1b04c7e5dd67f1b9708fe6ae667b681
xakyth <xakyth@gmail.com>
2023-06-20 07:04:35 +0300

    +test

--- src/test/java/com/xakyth/InversionsCountTest.java
+++ src/test/java/com/xakyth/InversionsCountTest.java
@@ -26,5 +26,7 @@ public class InversionsCountTest {
 
         assertEquals(0, InversionsCount.inversionsCount(new int[] { -1, 0, 1, 7, 10 }));
         assertEquals(4, InversionsCount.inversionsCount(new int[] { 11, 0, 1, 7, 10 }));
+        assertEquals(28, InversionsCount.inversionsCount(new int[] { 54044, 14108, 79294, 29649, 25260, 60660, 2995, 53777, 49689, 9083}));
+
     }
 }
